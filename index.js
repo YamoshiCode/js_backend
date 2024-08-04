f2b82c0f95cf821e69bac8283cab8099149c5579
Aman <aman@waterdip.ai>
2024-08-05 00:04:37 +0530

    homepage - 5

--- index.js
+++ index.js
@@ -2,7 +2,40 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT
-
+const gitdata = {
+    "login": "aman-waterdip",
+    "id": 162797207,
+    "node_id": "U_kgDOCbQWlw",
+    "avatar_url": "https://avatars.githubusercontent.com/u/162797207?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/aman-waterdip",
+    "html_url": "https://github.com/aman-waterdip",
+    "followers_url": "https://api.github.com/users/aman-waterdip/followers",
+    "following_url": "https://api.github.com/users/aman-waterdip/following{/other_user}",
+    "gists_url": "https://api.github.com/users/aman-waterdip/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/aman-waterdip/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/aman-waterdip/subscriptions",
+    "organizations_url": "https://api.github.com/users/aman-waterdip/orgs",
+    "repos_url": "https://api.github.com/users/aman-waterdip/repos",
+    "events_url": "https://api.github.com/users/aman-waterdip/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/aman-waterdip/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": null,
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 2,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2024-03-09T10:21:11Z",
+    "updated_at": "2024-07-30T17:01:14Z"
+  }
 app.get('/', (req, res) => {
   res.send(`
    <!DOCTYPE html>
@@ -52,11 +85,26 @@ app.get('/', (req, res) => {
 })
 
 
+app.get('/jokes' , (req, res) => {
+    const data = [
+        {"id": 1, "title": "A joke", "content": "This is a joke"},
+        {"id": 2, "title": "Another joke", "content": "This is another joke"},
+        {"id": 3, "title": "A third joke", "content": "This is a third joke"},
+        {"id": 4, "title": "A fourth joke", "content": "This is a fourth joke"},
+        {"id": 5, "title": "A fifth joke", "content": "This is a fifth joke"}
+    ]
+    res.json(data)
+})
+
 app.get('/check', (req, res)=> {
     res.send('check')
 })
 app.get('/aman' , (req,res) => {
     res.send(`aman choudhary`)
+})
+
+app.get('/github', (req,res)=> {
+    res.json(gitdata)
 })
 
 app.listen(port, () => {
